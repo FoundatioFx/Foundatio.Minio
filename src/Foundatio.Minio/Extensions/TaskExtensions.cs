b98d8a4f71cf379535dc1290fcaebabf35d6c3dc
Blake Niemyjski <bniemyjski@gmail.com>
2025-07-07 22:20:13 -0500

    Improves performance of AnyContext extension
    Adds AggressiveInlining to the AnyContext extension methods to potentially improve performance by encouraging the JIT compiler to inline these small, frequently used methods. This reduces the overhead of method calls.

--- src/Foundatio.Minio/Extensions/TaskExtensions.cs
+++ src/Foundatio.Minio/Extensions/TaskExtensions.cs
@@ -7,12 +7,14 @@ namespace Foundatio.Extensions;
 internal static class TaskExtensions
 {
     [DebuggerStepThrough]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ConfiguredTaskAwaitable<TResult> AnyContext<TResult>(this Task<TResult> task)
     {
         return task.ConfigureAwait(false);
     }
 
     [DebuggerStepThrough]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ConfiguredTaskAwaitable AnyContext(this Task task)
     {
         return task.ConfigureAwait(false);
